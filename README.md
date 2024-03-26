e654f52709892ec94f0f8fcb427ca380b6adc451
hallayus <hallayus@gmail.com>
2024-03-26 23:29:40 +0100

    initial url based search

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # coffee_map
 Generate a KML map containing speciality coffee shops in Europe
+
+# TODO:
+1. read from the actual ECT website name/address entries to make the search terms
+1. read the existing KML file into a dictionary and look up search terms there before doing the google search.
+1. setup github action to do a slow ECT scrape once a month
---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Create a KML file containing specialty coffee shops in Europe";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ]; # "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustc
+            cargo
+            rustfmt
+            pkg-config
+            gdb
+            openssl
+            xmlformat
+          ];
+          shellHook = ''
+            exec fish
+          '';
+        };
+      };
+    };
+}
